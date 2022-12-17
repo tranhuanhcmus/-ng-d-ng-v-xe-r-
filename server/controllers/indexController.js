8b588f17e3f6c4300a50f9978170c0650f778595
LucNguyen <lucmten@gmail.com>
2022-12-17 21:47:11 +0700

    Webdong

---
+++ server/controllers/indexController.js
@@ -0,0 +1,9 @@
+const controller = {}
+const title = 'HomePage';
+
+
+controller.showlist = (req, res) => {
+    res.render('Pages/homePage',{title});
+}
+
+module.exports = controller
--- server/routes/findtickedRoute.js
+++ server/routes/findtickedRoute.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controllers/indexController');
+const controller = require('../controllers/findtickedController');
 
 
 
