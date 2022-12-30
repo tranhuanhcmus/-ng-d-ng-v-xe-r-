e0605dcd01a604b0120bb5fcc53757e039cd0c23
thanh2231 <20127628@student.hcmus.edu.vn>
2022-12-30 15:06:16 +0700

    sửa code models

--- server/models/chuyenxe.js
+++ server/models/chuyenxe.js
@@ -11,10 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-<<<<<<< HEAD
+
       Chuyenxe.hasMany(models.Phieuxe, { foreignKey: 'chuyenxeId' });
       Chuyenxe.hasMany(models.CT_Chuyenxe, { foreignKey: 'chuyenxeId' });
-      Chuyenxe.hasMany(models.Xe, { foreignKey: 'chuyenxeId' });
+      Chuyenxe.belongsTo(models.Xe, { foreignKey: 'chuyenxeId' });
       Chuyenxe.belongsTo(models.Nhaxe, { foreignKey: 'nhaxeId' });
 
       Chuyenxe.hasMany(models.Thanhpho, { foreignKey: 'thanhphodiId' });
@@ -22,14 +22,10 @@ module.exports = (sequelize, DataTypes) => {
       Chuyenxe.hasMany(models.Chuyenxe, { foreignKey: 'chuyenxeId' });
     }
   }
-  Chuyenxe.init({
-    mota: DataTypes.STRING,
-=======
-    }
-  }
+
   Chuyenxe.init({
     motachinhsach: DataTypes.STRING,
->>>>>>> 57224f4c944fe1a950b0910d382a080f7100b550
+
     giave: DataTypes.INTEGER
   }, {
     sequelize,
