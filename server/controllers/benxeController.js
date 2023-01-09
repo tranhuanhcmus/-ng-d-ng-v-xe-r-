945c2d3909902e5397e6a317f0d7e551708441c7
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-01-09 11:55:28 +0700

    v1.2

--- server/controllers/benxeController.js
+++ server/controllers/benxeController.js
@@ -1,12 +1,8 @@
-
-
-const title = 'Testdatabase';
-const models = require('../models');
+const title = "Testdatabase";
+const models = require("../models");
 
 const controller = {
-
-    create: async (req, res) => {
-
+    create: async(req, res) => {
         try {
             const newbenxe = await models.Benxe(req.body);
             await newbenxe.save();
@@ -16,8 +12,7 @@ const controller = {
         }
     },
 
-    findAll: async (req, res) => {
-
+    findAll: async(req, res) => {
         try {
             const benxe = await models.Benxe.findAll();
             res.status(200).json(benxe);
@@ -26,61 +21,49 @@ const controller = {
         }
     },
     //tim điểm đón dựa vào ten bến xe
-    findOne: async (req, res) => {
-
+    findOne: async(req, res) => {
         try {
-            const benxe = await models.Benxe.findOne(
-                {
-                    where:
-                        { tenbenxe: req.params.tenbenxe }
-                },
-                {
-                    include: Thanhpho,
-                }
-            );
+            const benxe = await models.Benxe.findOne({
+                where: { tenbenxe: req.params.tenbenxe },
+            }, {
+                include: Thanhpho,
+            });
             res.status(200).json(benxe);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-    findByPk: async (req, res) => {
-
+    findByPk: async(req, res) => {
         try {
-            const benxe = awaitmodels.Benxe.findByPk(req.params.idbenxe)
+            const benxe = awaitmodels.Benxe.findByPk(req.params.idbenxe);
             res.status(200).json(benxe);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-    delete: async (req, res) => {
-
+    delete: async(req, res) => {
         try {
             const benxe = awaitmodels.Benxe.destroy({
-                where: { id: req.params.benxe }
-            })
+                where: { id: req.params.benxe },
+            });
             res.status(200).json(benxe);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-    update: async (req, res) => {
-
+    update: async(req, res) => {
         try {
-            const benxe = await models.Benxe.update(
-                {
-                    tenbenxe: req.params.tenbenxe,
-                    sdth: req.params.sdth,
-                    diachi: req.params.diachi
-                }, {
-                where: { id: req.params.idbenxe }
-            })
+            const benxe = await models.Benxe.update({
+                tenbenxe: req.params.tenbenxe,
+                sdth: req.params.sdth,
+                diachi: req.params.diachi,
+            }, {
+                where: { id: req.params.idbenxe },
+            });
             res.status(200).json(benxe);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-
-
-
-}
+};
 module.exports = controller;
