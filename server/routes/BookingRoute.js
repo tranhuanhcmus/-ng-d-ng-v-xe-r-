2d4735328c1452f848f2384a76bc61de008eb2fa
LucNguyen <lucmten@gmail.com>
2023-01-11 21:32:33 +0700

    commit booking query

--- server/routes/BookingRoute.js
+++ server/routes/BookingRoute.js
@@ -3,11 +3,11 @@ var router = express.Router();
 const request = require("request-promise");
 // const controller = require('../controllers/BookingController');
 const title = 'BookingPage';
-const data = [{
-  img: './images/Booking/garage.png',
-  type: 'Limousin giường phòng 40 chỗ', id: '',
-  tennhaxe: "Huan Bs", tpdi: "vp Sài Gòn", tpden: "VP Bình Định", tgkhoihanh: "12:00", tgketthuc: "00:00", ngaykhoihanh: "20/09/2023", giave: "500.000"
-}];
+// const data = [{
+//   img: './images/Booking/garage.png',
+//   type: 'Limousin giường phòng 40 chỗ', id: '',
+//   tennhaxe: "Huan Bs", tpdi: "vp Sài Gòn", tpden: "VP Bình Định", tgkhoihanh: "12:00", tgketthuc: "00:00", ngaykhoihanh: "20/09/2023", giave: "500.000"
+// }];
 const Brand = ['Nhà xe Thành Bưởi', 'Nhà Xe Phương Trang', 'Nhà xe Phương Tân Niên', 'Nhà xe Tuấn Hưng'];
 const Place = ['TP Cà Mau', 'TP Hồ Chí Minh', 'TP Đà Nẵng', 'TP Hà Nội'];
 const TypeBed = ['Xe giường nằm', 'Xe giường phòng', 'Xe thường'];
